66d330e43d4d616bfaa8c99d695e3115b9f67909
Jared Bruni <jaredbruni@gmail.com>
2026-05-04 05:18:03 -0700

    icon / resource files

--- game.c
+++ game.c
@@ -958,6 +958,11 @@ int main(int argc, char **argv) {
         WIDTH = atoi(argv[2]);
         HEIGHT = atoi(argv[3]);
     }
+
+#ifdef DEFAULT_FULL
+    full = 1;
+#endif
+
     if(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_JOYSTICK | SDL_INIT_TIMER) < 0)
         return -1;
 
